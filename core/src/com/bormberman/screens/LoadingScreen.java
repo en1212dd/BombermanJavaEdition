2abe6f107366fa562a82b65bce34a5f6ac7838f5
Yerko Josue Alvarezé <yerkojue@gmail.com>
2022-05-07 21:00:14 +0200

    archivos para la UI y correcciones

--- core/src/com/bormberman/screens/LoadingScreen.java
+++ core/src/com/bormberman/screens/LoadingScreen.java
@@ -1,8 +1,6 @@
 package com.bormberman.screens;
 
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.utils.ScreenUtils;
